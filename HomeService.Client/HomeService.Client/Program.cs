cb51814533a0f13e098d259424d0b1b59f622db7
Zefirrat <leonidpavlov314@gmail.com>
2021-03-23 09:22:27 +0500

    First steps working

---
+++ HomeService.Client/HomeService.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using HomeService.Server;
+
+namespace HomeService.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001",
+                new GrpcChannelOptions {HttpHandler = httpHandler});
+            
+            var greeterClient = new Greeter.GreeterClient(channel);
+            var greeterReply = await greeterClient.SayHelloAsync(
+                new HelloRequest {Name = "GreeterClient"});
+            Console.WriteLine($"Greeting: {greeterReply.Message}");
+
+            var indicatorClient = new Indicator.IndicatorClient(channel);
+            var indicatorReply = await indicatorClient.IndicateAsync(
+                new IndicateRequest {Color = 1, Duration = 200});
+            Console.WriteLine($"Indicator: {indicatorReply.Indicated}");
+            
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeService.Server/HomeService.Server/Services/IndicatorService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace HomeService.Server
+{
+    public class IndicatorService : Indicator.IndicatorBase
+    {
+        public IndicatorService(ILogger<IndicatorService> logger)
+        {
+            _logger = logger;
+        }
+
+        private ILogger _logger { get; }
+
+        public override Task<IndicateReply> Indicate(IndicateRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new IndicateReply {Indicated = true});
+        }
+    }
+}
